f7cc049312a2d26a254b2866b116ca71e833c6b2
Omar Atcho <omar@Omars-MacBook-Pro.local>
2019-02-11 16:15:27 -0500

    added animal profile class, working on setting up shelter class

---
+++ src/com/company/AnimalProfile.java
@@ -0,0 +1,87 @@
+package com.company;
+
+public class AnimalProfile {
+    private int idNumber;
+    private String name;
+    private String typeOfAnimal;
+    private String breed;
+    private String color;
+    private int age;
+    private String arrivalDate;
+    private String status;
+
+    public AnimalProfile (int idNumber, String name, String typeOfAnimal, String breed, String color, int age, String arrivalDate, String status){
+        this.idNumber = idNumber;
+        this.name = name;
+        this.typeOfAnimal = typeOfAnimal;
+        this.breed = breed;
+        this.color = color;
+        this.age = age;
+        this.arrivalDate = arrivalDate;
+        this.status = status;
+    }
+
+    public int getIdNumber() {
+        return idNumber;
+    }
+
+    public void setIdNumber(int idNumber) {
+        this.idNumber = idNumber;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getTypeOfAnimal() {
+        return typeOfAnimal;
+    }
+
+    public void setTypeOfAnimal(String typeOfAnimal) {
+        this.typeOfAnimal = typeOfAnimal;
+    }
+
+    public String getBreed() {
+        return breed;
+    }
+
+    public void setBreed(String breed) {
+        this.breed = breed;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getArrivalDate() {
+        return arrivalDate;
+    }
+
+    public void setArrivalDate(String arrivalDate) {
+        this.arrivalDate = arrivalDate;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+}
